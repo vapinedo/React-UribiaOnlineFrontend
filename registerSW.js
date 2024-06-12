610f274d662e543000ac6fabec259633ee5f63ec
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-06-12 14:06:18 -0500

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/React-UribiaOnlineFrontend/sw.js', { scope: '/React-UribiaOnlineFrontend/' })})}
