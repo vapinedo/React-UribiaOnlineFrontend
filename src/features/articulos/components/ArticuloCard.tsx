db72089909692ad80149e84b7baa910cdab11392
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-06-12 15:33:53 -0500

    agregar articulo page

--- src/features/articulos/components/ArticuloCard.tsx
+++ src/features/articulos/components/ArticuloCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Articulo } from "@features/articulos/models/Articulo";
 
 declare module '@features/articulos/models/Articulo' {
@@ -12,11 +13,16 @@ interface ArticuloCardProps {
 }
 
 const ArticuloCard: React.FC<ArticuloCardProps> = ({ articulo }) => {
+  const navigate = useNavigate();
   const barrioNombre = articulo.barrioNombre || '';
+
+  const handleDetail = () => {
+    navigate(`/articulo/${articulo.id}`);
+  };
 
   const handleShare = () => {
     const articleUrl = encodeURIComponent(window.location.href);
-    console.log({articleUrl});
+    console.log({ articleUrl });
     const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${articleUrl}`;
     window.open(facebookShareUrl, '_blank');
   };
@@ -27,10 +33,14 @@ const ArticuloCard: React.FC<ArticuloCardProps> = ({ articulo }) => {
       <div className="card-body">
         <p className="text-muted" style={{ fontSize: "1.3rem", marginBottom: "10px" }}>{articulo.nombre}</p>
         <h4 className="card-text">$ {articulo.precio}</h4>
-        <p className="card-text badge text-bg-primary">{articulo.estadoArticulo}</p>
+        <p className="card-text badge text-bg-success">{articulo.estadoArticulo}</p>
         <p className="card-text mb-2">Barrio {barrioNombre}</p>
         <small className="text-muted">Publicado hace 1 hora</small>
         <hr />
+
+        <button className="btn btn-sm btn-primary" style={{ marginRight: "10px" }} onClick={handleDetail}>
+          Ver Detalles
+        </button>
 
         <button className="btn btn-sm btn-outline-primary" onClick={handleShare}>
           <i className='bx bxl-facebook'></i> Compartir
